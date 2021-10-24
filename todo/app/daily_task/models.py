9f56e1b90b70cc75e6ebee20572a426540620fe3
Rosemary Rajan <rosemarymannattu@gmail.com>
2021-10-24 15:46:37 +0530

    Direct db connection completed

--- todo/app/daily_task/models.py
+++ todo/app/daily_task/models.py
@@ -5,7 +5,20 @@ def create_task(task, description, date):
     conn = connect_db()
     cur = create_cursor(conn)
 
-    res = cur.execute("INSERT INTO task (task, description, date) VALUES (task, description, date);")
+    res = cur.execute("INSERT INTO task(task, description, date) VALUES (task, description, date);")
+    # Make the changes to the database persistent
+    conn = commit_db(conn)
+    # Close communication with the database
+    close_cursor(cur)
+    close_db(conn)
+    return res
+
+
+def get_task_list():
+    conn = connect_db()
+    cur = create_cursor(conn)
+
+    res = cur.execute("SELECT * FROM task;")
     # Make the changes to the database persistent
     conn = commit_db(conn)
     # Close communication with the database
--- todo/app/daily_task/urls.py
+++ todo/app/daily_task/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-
+from app.daily_task.views import TaskView
 
 urlpatterns = [
-    # path('home-page/', HomePageView.as_view(), name='home-page'),
+    path('tasks/', TaskView.as_view(), name='tasks'),
 ]
